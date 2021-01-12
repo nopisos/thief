971f42457c87f89e8d3bc89609e37b979d110af3
nopisos <77279082+nopisos@users.noreply.github.com>
2021-01-12 11:53:08 +0300

    b

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.Translate(2 * Time.deltaTime, 0, 0);
+    }
+}
+
---
+++ Assets/scripts/signalingTrigger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class signalingTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource _sound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        VolumeUp();
+        StartVolumeUp();
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StopVolumeUp();
+        StartVolumeReduce();
+    }
+
+    public void StartVolumeUp()
+    {
+        var VolumeUpJob = StartCoroutine(VolumeUp());
+    }
+
+    private void StopVolumeUp()
+    {
+        StopCoroutine(VolumeUp());
+    }
+
+    private void StartVolumeReduce()
+    {
+        var VolumeReduceJob = StartCoroutine(VolumeReduce());        
+    }
+
+    private IEnumerator VolumeUp()
+    {
+        float _volume = _sound.volume;
+        _sound.volume = 0;
+        _sound.Play();
+
+        float step = 0.1f;
+        float i = 0;
+        while (i <= _volume)
+        {
+            i += Time.deltaTime * step;
+            _sound.volume = i;
+            yield return null;
+        }
+    }
+
+    private IEnumerator VolumeReduce()
+    {
+        float step = 0.1f;
+        float i = _sound.volume;
+        while (i >= 0.05f)
+        {
+            i -= Time.deltaTime * step;
+            _sound.volume = i;
+            yield return null;
+        }
+    }
+
+
+}
